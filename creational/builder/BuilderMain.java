4453a48ff81983fe2dd644a50ec864ffee123dfa
Stefano Marcolini <39852020+stefanomarcolini@users.noreply.github.com>
2019-03-30 21:40:16 +0100

    Update BuilderMain.java

--- creational/builder/BuilderMain.java
+++ creational/builder/BuilderMain.java
@@ -8,13 +8,13 @@ public class BuilderMain {
 
 		@SuppressWarnings("static-access")
 		Built built = Builder.setInt(2)
-							 .setFloat((float) Math.PI)
-							 .setByte((byte) 12)
-							 .setShort((short) 6500)
-							 .setDouble(Math.E)
-							 .setLong(987654321098765432L)
-							 .setString("Ready to build...")
-							 .build();
+				     .setFloat((float) Math.PI)
+				     .setByte((byte) 12)
+				     .setShort((short) 6500)
+				     .setDouble(Math.E)
+				     .setLong(987654321098765432L)
+				     .setString("Ready to build...")
+				     .build();
 		
 		System.out.println(built.toString());
 		
