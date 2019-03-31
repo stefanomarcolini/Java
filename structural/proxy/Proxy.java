0e5cd274c139054cd0c14497f1dc591fe55e38e0
Stefano Marcolini <39852020+stefanomarcolini@users.noreply.github.com>
2019-03-31 22:40:16 +0200

    Add files via upload

---
+++ structural/proxy/Proxy.java
@@ -0,0 +1,120 @@
+package design.pattern.structural.proxy;
+
+import java.util.LinkedList;
+
+/**
+ * Proxy implementation:
+ * the proxy is responsible to manage the business logic
+ * used to read, write and delete each Message based on
+ * the Person's login privilege.
+ * */
+public class Proxy {
+
+	private Login login;
+	private LinkedList<Message> messages;
+	
+	public Proxy() {
+		messages = new LinkedList<>();
+	}
+
+	public void login(Person person) {
+		this.login = new Login(person);
+		login.login(login.person.name, login.person.privileges);
+	}
+	
+	public void write(String message) {
+		Message msg = new Message(message);
+		messages.add(msg);
+	}
+
+	public void read() {
+		if (messages == null || messages.isEmpty()) {
+			System.out.println("There are no messages yet!");
+		} else {
+
+			for (Message message : messages) {
+				System.out.println(String.format("%d >> %s", message.ID, message.message));
+			}
+		}
+	}
+
+	/**
+	 * only ADMINs can delete messages from the list
+	 * */
+	public void delete(Person person, long id) {
+
+		if (messages == null || messages.isEmpty()) {
+			System.out.println("There are no messages yet!");
+		} else {
+			for (Message message : messages) {
+				if (Privileges.ADMIN.equals(person.privileges) && message.ID == id) {
+					if (messages.remove(message)) {
+						System.out.println(String.format("Message %d >> DELETED!", message.ID));
+						break;
+					} else {
+						System.err.println("Error: Unable to Delete message");
+						break;
+					}
+				} else {
+					System.out.println(String.format("Sorry, you can't delete message %d as you are logged in as %s", id, person.privileges));
+					break;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * inner public Person class
+	 * */
+	public class Person {
+		
+		private String name;
+		private Privileges privileges;
+		
+		public Person(String name, Privileges privileges) {
+			this.name = name;
+			this.privileges = privileges;
+		}
+		
+	}
+	
+	/**
+	 * inner private Login class
+	 * */
+	private class Login {
+
+		private Person person;
+		
+		private Login(Person person) {
+			this.person = person;
+		}
+
+		private void login(String name, Privileges privileges) {
+			System.out.println(String.format("Wellcome %s, you have logged in as %s", name, privileges));
+		}
+
+	}
+	
+	/**
+	 * inner private Message class
+	 * */
+	private static class Message {
+
+		private String message;
+		private static long number;
+		private final long ID;
+
+		private Message(String message) {
+			ID = ++number;
+			this.message = message;
+		}
+
+	}
+	
+	/**
+	 * inner public Privileges enum
+	 * */
+	public enum Privileges {
+		USER, ADMIN;
+	}
+}
---
+++ structural/proxy/ProxyMain.java
@@ -0,0 +1,50 @@
+package design.pattern.structural.proxy;
+
+import design.pattern.structural.proxy.Proxy.Person;
+import design.pattern.structural.proxy.Proxy.Privileges;
+
+public class ProxyMain {
+
+	public static void main(String[] args) {
+		
+		Proxy proxy = new Proxy();
+		
+		Person[] persons = new Person[2];
+		persons[0] = proxy.new Person("Alice", Privileges.ADMIN);
+		persons[1] = proxy.new Person("Bob", Privileges.USER);
+		
+		
+		proxy.login(persons[0]);
+		
+		proxy.read();
+		proxy.write("Hello World!");
+		proxy.read();
+		proxy.delete(persons[0], 1L);
+		
+		proxy.login(persons[1]);
+		
+		proxy.read();
+		proxy.write("Hi there");
+		proxy.read();
+		proxy.delete(persons[1], 1L);
+		
+		proxy.delete(persons[0], 2L);
+		proxy.read();
+		
+		/*	OUTPUT:
+		 * 
+		 * 	Wellcome Alice, you have logged in as ADMIN
+		 * 	There are no messages yet!
+		 * 	1 >> Hello World!
+		 * 	Message 1 >> DELETED!
+		 * 	Wellcome Bob, you have logged in as USER
+		 * 	There are no messages yet!
+		 * 	2 >> Hi there
+		 * 	Sorry, you can't delete message 1 as you are logged in as USER
+		 * 	Message 2 >> DELETED!
+		 * 	There are no messages yet!
+		 * */
+		
+	}
+
+}
